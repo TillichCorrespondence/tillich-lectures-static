f214204715bef332b796ad772c44f5ea161d43d4
csae8092 <p.andorfer@gmail.com>
2024-12-20 14:38:02 +0100

    fetch indices data

--- fetch_data.sh
+++ fetch_data.sh
@@ -10,8 +10,24 @@ mv ./tillich-lectures-data-main/data/ .
 rm main.zip
 rm -rf ./tillich-lectures-data-main
 
+echo "fetching indices from tillich-entities"
+rm -rf data/indices
+curl -LO https://github.com/TillichCorrespondence/tillich-entities/archive/refs/heads/main.zip
+unzip main
+rm main.zip
+mkdir ./data/indices
+mv ./tillich-entities-main/data/indices ./data
+rm -rf tillich-entities-main
+
+rm ./data/indices/listbibl.xml
+wget https://raw.githubusercontent.com/TillichCorrespondence/tillich-zotero/refs/heads/main/listbibl.xml -P ./data/indices
+
 add-attributes -g "./data/editions/*.xml" -b "https://tillich-lectures.acdh.oeaw.ac.at"
 add-attributes -g "./data/meta/*.xml" -b "https://tillich-lectures.acdh.oeaw.ac.at"
+add-attributes -g "./data/indices/*.xml" -b "https://tillich-lectures.acdh.oeaw.ac.at"
+
+echo "denormalizing indices" 
+denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml"
 
 echo "fetching imprint data"
 ./shellscripts/dl_imprint.sh
