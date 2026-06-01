01310465c5ffe6d4d367c119bbb0a87517d6c0cf
csae8092 <p.andorfer@gmail.com>
2026-06-01 15:13:04 +0200

    wip

--- pyscripts/make_bible_index.py
+++ pyscripts/make_bible_index.py
@@ -1,28 +1,40 @@
 import glob
-from collections import defaultdict
+import os
 
 import lxml.etree as ET
 from acdh_tei_pyutils.tei import TeiReader
-from acdh_tei_pyutils.utils import check_for_hash
+from acdh_tei_pyutils.utils import check_for_hash, extract_fulltext
 from slugify import slugify
 
 NS = "http://www.tei-c.org/ns/1.0"
 
 files = sorted(glob.glob("./data/*/*xml"))
-d = defaultdict(set)
+data = {}
 
 for x in files:
     doc = TeiReader(x)
     title = doc.any_xpath(".//tei:titleStmt/tei:title[1]/text()")[0]
-    xml_id = doc.any_xpath("./@xml:id")[0]
+    xml_id = os.path.split(x)[-1]
     for rs in doc.any_xpath(".//tei:rs[@type='bible' and @ref]"):
-        ref = rs.get("ref")
-        quote_text = "".join(rs.itertext()).strip()
+        ref = rs.attrib["ref"]
         key = check_for_hash(ref)
-        new_key = f"tl-bible-id__{slugify(key)}"
+        rs.attrib["n"] = key
+        if key.startswith("tl-bible-id__"):
+            new_key = key
+        else:
+            new_key = f"tl-bible-id__{slugify(key)}"
+
+        try:
+            data[new_key]["docs"][xml_id].add(extract_fulltext(rs))
+            data[new_key]["label"] = key
+        except KeyError:
+            data[new_key] = {"label": key, "docs": {xml_id: set()}}
+            data[new_key]["docs"][xml_id].add(extract_fulltext(rs))
         rs.attrib["ref"] = f"#{new_key}"
-        d[new_key].add(f"{key}|{title}|{quote_text}")
     doc.tree_to_file(x)
+
+
+print(data)
 
 tei_dummy = """
 <?xml version='1.0' encoding='UTF-8'?>
@@ -51,42 +63,23 @@ tei_dummy = """
 doc = TeiReader(tei_dummy)
 root = doc.any_xpath(".//tei:list")[0]
 
-# --------------------------------------------------
-# BUILD LIST ITEMS
-# --------------------------------------------------
-for key, value in sorted(d.items()):
-    label = list(value)[0].split("|")[0]
-    print(value)
 
-    item = ET.Element(f"{{{NS}}}item")
-    root.append(item)
+for key, value in data.items():
+    item = ET.Element("{http://www.tei-c.org/ns/1.0}item")
 
-    # ---- term ----
-    term = ET.Element(f"{{{NS}}}term")
+    term = ET.SubElement(item, "{http://www.tei-c.org/ns/1.0}term")
     term.attrib["{http://www.w3.org/XML/1998/namespace}id"] = key
-    term.text = label
-    item.append(term)
+    term.text = value["label"]
 
-    # ---- noteGrp ----
-    notegrp = ET.Element(f"{{{NS}}}noteGrp")
-    item.append(notegrp)
-
-    for xml_id, src_title, citation in (v.split("|", 2) for v in sorted(value)):
-        note = ET.Element(f"{{{NS}}}note")
+    notegrp = ET.SubElement(item, "{http://www.tei-c.org/ns/1.0}noteGrp")
+    for xml_id, quote in value["docs"].items():
+        note = ET.SubElement(notegrp, "{http://www.tei-c.org/ns/1.0}note")
         note.attrib["type"] = "mentions"
         note.attrib["target"] = xml_id
-
-        # title text
-        note.text = src_title + " "
-
-        # citation inside note
-        cit = ET.Element(f"{{{NS}}}cit")
-        quote = ET.Element(f"{{{NS}}}quote")
-        quote.text = citation
-        cit.append(quote)
-        note.append(cit)
-
-        notegrp.append(note)
-
+        for y in quote:
+            cit = ET.SubElement(note, "{http://www.tei-c.org/ns/1.0}cit")
+            quote = ET.SubElement(cit, "{http://www.tei-c.org/ns/1.0}quote")
+            quote.text = y
+    root.append(item)
 
 doc.tree_to_file("./data/indices/listbible.xml")
